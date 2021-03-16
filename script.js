134f16e8112a53b216ae56ad882dfdbab49817d3
TomikBoi <toms.kedo@gmail.com>
2021-03-16 19:50:08 +0200

    Added reset feature

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.again').addEventListener('click', function() {
+  
+  score = 20;
 
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.message').textContent = 'Start guessing...'
+
+  document.querySelector('.number').style.width = '15rem'
+})
 
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value)
@@ -17,6 +32,13 @@ document.querySelector('.check').addEventListener('click', function() {
     // When player wins 
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!'
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem'
+
     // When guess is too high
   } else if (guess > secretNumber) {
     if(score > 1) {
