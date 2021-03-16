bee08c589f5fc894a24d183aa4f92ec7ba318918
TomikBoi <toms.kedo@gmail.com>
2021-03-16 19:58:21 +0200

    added highscore / finished game

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.again').addEventListener('click', function() {
   
@@ -38,6 +39,11 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem'
+
+    if(score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
